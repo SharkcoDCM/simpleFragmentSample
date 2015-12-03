3f98399724d1b24e4967af48157064cbff4dc656
MohammadTazehkar <mohammad.tazehkar@gmail.com>
2015-12-03 21:45:53 +0330

    Simple Fragment Sample

---
+++ app/src/main/java/com/example/mohammad/fragment/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mohammad.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fr = new fragment();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+    }
+}
+
+
